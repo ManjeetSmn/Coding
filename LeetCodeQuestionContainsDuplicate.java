572e0fafddeb1db09bb658cde21529d4be5f5d3b
ManjeetSmn <40335006+ManjeetSmn@users.noreply.github.com>
2023-12-29 00:18:49 +0530

    Create LeetCodeQuestionContainsDuplicate.java

---
+++ LeetCodeQuestionContainsDuplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+      //Link to question https://leetcode.com/problems/contains-duplicate/
+        Set<Integer> integerSet = new HashSet<Integer>(); //an unordered collection or list, where duplicate values are not allowed
+        for (int i = 0; i < nums.length; i++) {
+            if (integerSet.contains(nums[i]))
+            {
+                return true;
+            }
+            else {
+                integerSet.add(nums[i]);
+            }
+        }
+         return false;
+    }
+   
+}
